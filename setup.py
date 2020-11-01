bc6f11bcaa6ea8d77c17875bbb075da80247fa84
davidmezzetti <561939+davidmezzetti@users.noreply.github.com>
2020-11-01 17:05:09 -0500

    Bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
     DESCRIPTION = f.read()
 
 setup(name="txtai",
-      version="1.3.0",
+      version="1.4.0",
       author="NeuML",
       description="AI-powered search engine",
       long_description=DESCRIPTION,
