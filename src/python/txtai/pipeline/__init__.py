8fbbc8da580a1ee867db85539acfbc546a65959e
davidmezzetti <561939+davidmezzetti@users.noreply.github.com>
2021-03-17 15:48:57 -0400

    Add additional imports

--- src/python/txtai/pipeline/__init__.py
+++ src/python/txtai/pipeline/__init__.py
@@ -4,8 +4,14 @@ Pipeline imports
 
 from .base import Pipeline
 from .extractor import Extractor
+from .hfmodel import HFModel
 from .hfpipeline import HFPipeline
 from .labels import Labels
 from .questions import Questions
 from .similarity import Similarity
+from .summary import Summary
+from .tensors import Tensors
+from .textractor import Textractor
 from .tokenizer import Tokenizer
+from .transcription import Transcription
+from .translation import Translation
