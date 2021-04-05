cc4eff89b4d13edc49d8e33e93ad7021ec7e00b1
davidmezzetti <561939+davidmezzetti@users.noreply.github.com>
2021-04-04 21:00:33 -0400

    Add UTF-8 encoding when getting README

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 # pylint: disable = C0111
 from setuptools import find_packages, setup
 
-with open("README.md", "r") as f:
+with open("README.md", "r", encoding="utf-8") as f:
     DESCRIPTION = f.read()
 
 setup(
