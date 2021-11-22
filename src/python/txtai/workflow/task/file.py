82fd7991395f7d3ace16c989652a832ba2b9fa73
davidmezzetti <561939+davidmezzetti@users.noreply.github.com>
2021-11-22 14:57:11 -0500

    Replace leading file:// urls in FileTask

--- src/python/txtai/workflow/task/file.py
+++ src/python/txtai/workflow/task/file.py
@@ -3,6 +3,7 @@ FileTask module
 """
 
 import os
+import re
 
 from .base import Task
 
@@ -12,6 +13,16 @@ class FileTask(Task):
     Task that processes file paths
     """
 
+    # File prefix
+    FILE = r"file:\/\/"
+
     def accept(self, element):
+        # Replace file prefixes
+        element = re.sub(FileTask.FILE, "", element)
+
         # Only accept file paths that exist
         return super().accept(element) and isinstance(element, str) and os.path.exists(element)
+
+    def prepare(self, element):
+        # Replace file prefixes
+        return re.sub(FileTask.FILE, "", element)
