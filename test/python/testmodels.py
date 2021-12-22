d53f1f5b1094d25346452560f9758a0121398445
davidmezzetti <561939+davidmezzetti@users.noreply.github.com>
2021-12-22 07:35:47 -0500

    Add test for models module

---
+++ test/python/testmodels.py
@@ -0,0 +1,33 @@
+"""
+Models module tests
+"""
+
+import unittest
+
+from unittest.mock import patch
+
+from txtai.models import Models
+
+
+class TestModels(unittest.TestCase):
+    """
+    Models tests.
+    """
+
+    @patch("torch.cuda.is_available")
+    def testDeviceid(self, cuda):
+        """
+        Tests the deviceid method
+        """
+
+        cuda.return_value = True
+        self.assertEqual(Models.deviceid(True), 0)
+        self.assertEqual(Models.deviceid(False), -1)
+        self.assertEqual(Models.deviceid(0), 0)
+        self.assertEqual(Models.deviceid(1), 1)
+
+        cuda.return_value = False
+        self.assertEqual(Models.deviceid(True), -1)
+        self.assertEqual(Models.deviceid(False), -1)
+        self.assertEqual(Models.deviceid(0), -1)
+        self.assertEqual(Models.deviceid(1), -1)
