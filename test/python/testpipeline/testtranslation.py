b68d8736c4cab0eb6d59ce01b2d47633cdd6332d
davidmezzetti <561939+davidmezzetti@users.noreply.github.com>
2022-01-27 18:12:59 -0500

    Skip test for macOS

--- test/python/testpipeline/testtranslation.py
+++ test/python/testpipeline/testtranslation.py
@@ -3,6 +3,7 @@ Translation module tests
 """
 
 import os
+import platform
 import unittest
 
 from txtai.pipeline import Translation
@@ -26,7 +27,7 @@ class TestTranslation(unittest.TestCase):
         # Validate translation text
         self.assertIsNotNone(translation)
 
-    @unittest.skipIf(os.name == "nt", "M2M100 skipped on Windows")
+    @unittest.skipIf(os.name == "nt" or platform.system() == "Darwin", "M2M100 skipped on Windows and macOS")
     def testM2M100Translation(self):
         """
         Tests a translation using M2M100 models
