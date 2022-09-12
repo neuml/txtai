4f479056b4e05e79edd6adfc222956799e834195
davidmezzetti <561939+davidmezzetti@users.noreply.github.com>
2022-09-12 10:07:21 -0400

    Bump version

--- src/python/txtai/__init__.py
+++ src/python/txtai/__init__.py
@@ -8,7 +8,7 @@ import logging
 logging.basicConfig(format="%(asctime)s [%(levelname)s] %(funcName)s: %(message)s")
 
 # Current version tag
-__version__ = "4.7.0"
+__version__ = "5.0.0"
 
 # Current pickle protocol
 __pickle__ = 4
