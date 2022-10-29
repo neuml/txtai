5233bce29d80256c2da01730389a5df7e0f75088
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-29 13:34:44 +0000

    Adding tarfile member sanitization to extractall()

--- src/python/txtai/graph/base.py
+++ src/python/txtai/graph/base.py
@@ -291,7 +291,26 @@ class Graph:
         with TemporaryDirectory() as directory:
             # Extract tar contents
             with tarfile.open(path, "r") as tar:
-                tar.extractall(directory)
+                def is_within_directory(directory, target):
+                    
+                    abs_directory = os.path.abspath(directory)
+                    abs_target = os.path.abspath(target)
+                
+                    prefix = os.path.commonprefix([abs_directory, abs_target])
+                    
+                    return prefix == abs_directory
+                
+                def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                
+                    for member in tar.getmembers():
+                        member_path = os.path.join(path, member.name)
+                        if not is_within_directory(path, member_path):
+                            raise Exception("Attempted Path Traversal in Tar File")
+                
+                    tar.extractall(path, members, numeric_owner=numeric_owner) 
+                    
+                
+                safe_extract(tar, directory)
 
             # Load graph backend
             self.loadgraph(f"{directory}/graph")
