c8ccf5fd8977903f169b11ca8afc4799f995ba57
Azam Din <46710015+adin786@users.noreply.github.com>
2022-12-12 14:25:36 +0000

    Removed root logger configuration as this should be handled by application code

--- src/python/txtai/__init__.py
+++ src/python/txtai/__init__.py
@@ -4,8 +4,13 @@ Version string
 
 import logging
 
-# Set default logging format
-logging.basicConfig(format="%(asctime)s [%(levelname)s] %(funcName)s: %(message)s")
+# Configure logging for the txtai package
+# We should use logging.basicConfig() here as it modifies the root logger.  But
+# log handlers/formatters should only be tweaked in the application code.  Ref:
+#   - https://stackoverflow.com/a/27017068/19357935
+#   - https://docs.python.org/3/howto/logging.html#configuring-logging-for-a-library
+logger = logging.getLogger(__name__)
+logger.addHandler(logging.NullHandler())
 
 # Current version tag
 __version__ = "5.2.0"
