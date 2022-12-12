11ab5ac8abe29796830ff043968b19e6984997e4
Azam Din <46710015+adin786@users.noreply.github.com>
2022-12-12 15:23:40 +0000

    Summarised comment lines to keep the code tidy

--- src/python/txtai/__init__.py
+++ src/python/txtai/__init__.py
@@ -4,11 +4,7 @@ Version string
 
 import logging
 
-# Configure logging for the txtai package
-# We should use logging.basicConfig() here as it modifies the root logger.  But
-# log handlers/formatters should only be tweaked in the application code.  Ref:
-#   - https://stackoverflow.com/a/27017068/19357935
-#   - https://docs.python.org/3/howto/logging.html#configuring-logging-for-a-library
+# Configure logging per standard Python library recommendations
 logger = logging.getLogger(__name__)
 logger.addHandler(logging.NullHandler())
 
