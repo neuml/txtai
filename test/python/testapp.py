e2e90366395614637ce53fd20caf5140d80156a0
davidmezzetti <561939+davidmezzetti@users.noreply.github.com>
2023-01-24 10:04:38 -0500

    Add clear error when starting API and config file not found, closes #412

---
+++ test/python/testapp.py
@@ -0,0 +1,21 @@
+"""
+Application module tests
+"""
+
+import unittest
+
+from txtai.app import Application
+
+
+class TestApp(unittest.TestCase):
+    """
+    Application tests.
+    """
+
+    def testConfig(self):
+        """
+        Tests a file not found config exception
+        """
+
+        with self.assertRaises(FileNotFoundError):
+            Application.read("No file here")
