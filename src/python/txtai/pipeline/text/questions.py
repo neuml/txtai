96c0d24ab84402ead81e369b39e14e35a996f092
Ryan Cali <ryancali@PaulThoRyansMBP.fios-router.home>
2023-06-02 12:45:21 -0700

    Changed topk to call

--- src/python/txtai/pipeline/text/questions.py
+++ src/python/txtai/pipeline/text/questions.py
@@ -10,10 +10,11 @@ class Questions(HFPipeline):
     Runs extractive QA for a series of questions and contexts.
     """
 
-    def __init__(self, path=None, quantize=False, gpu=True, model=None, top_k=1):
+    def __init__(self, path=None, quantize=False, gpu=True, model=None):
         super().__init__("question-answering", path, quantize, gpu, model)
-        self.top_k = top_k
-    def __call__(self, questions, contexts, workers=0):
+     
+    def __call__(self, questions, contexts, workers=0, topk=1):
+        self.top_k = topk
         """
         Runs a extractive question-answering model against each question-context pair, finding the best answers.
 
@@ -29,22 +30,22 @@ class Questions(HFPipeline):
         for x, question in enumerate(questions):
             if question and contexts[x]:
                 # Run the QA pipeline
-                results = self.pipeline(question=question, context=contexts[x], num_workers=workers, top_k=self.top_k)
-                answer_list = []
+                results = self.pipeline(question=question, context=contexts[x], num_workers=workers, topk=self.top_k)
+                answerlist = []
                 # This is for handling lists of answers
                 if isinstance(results, list):
                     for res in results:
                         # Get answer and score
                         answer, score = res["answer"], res["score"]
                         if score >= 0.05:
-                            answer_list.append(answer)
+                            answerlist.append(answer)
                 else:  # This is for handling a single answer
                     answer, score = results["answer"], results["score"]
                     if score >= 0.05:
-                        answer_list.append(answer)
+                        answerlist.append(answer)
                 # Add answers
-                if answer_list:
-                    answers.append(answer_list)
+                if answerlist:
+                    answers.append(answerlist)
                 else:
                     answers.append(None)
             else:
