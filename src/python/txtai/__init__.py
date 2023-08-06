da4290900735693bc5e489a1964149299512dfdd
davidmezzetti <561939+davidmezzetti@users.noreply.github.com>
2023-08-06 19:18:40 -0400

    Add top level imports, closes #514

--- src/python/txtai/__init__.py
+++ src/python/txtai/__init__.py
@@ -13,3 +13,8 @@ __version__ = "6.0.0"
 
 # Current pickle protocol
 __pickle__ = 4
+
+# pylint: disable=C0413
+# Top-level imports, must run after variables above defined
+from .app import Application
+from .embeddings import Embeddings
