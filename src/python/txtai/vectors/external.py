12acc87e372c96fe8f54fdc34f31cd33e8ccb077
davidmezzetti <561939+davidmezzetti@users.noreply.github.com>
2024-01-29 15:25:15 -0500

    Resolve external vector transform functions, closes #650

--- src/python/txtai/vectors/external.py
+++ src/python/txtai/vectors/external.py
@@ -2,7 +2,11 @@
 External module
 """
 
+import types
+
 import numpy as np
+
+from ..util import Resolver
 
 from .base import Vectors
 
@@ -13,14 +17,39 @@ class ExternalVectors(Vectors):
     test different model configurations without having to recompute vectors.
     """
 
+    def __init__(self, config, scoring):
+        super().__init__(config, scoring)
+
+        # Lookup and resolve transform function
+        self.transform = self.resolve(config.get("transform"))
+
     def load(self, path):
         return None
 
     def encode(self, data):
         # Call external transform function, if available and data not already an array
-        transform = self.config.get("transform")
-        if transform and data and not isinstance(data[0], np.ndarray):
-            data = transform(data)
+        if self.transform and data and not isinstance(data[0], np.ndarray):
+            data = self.transform(data)
 
         # Cast to float32
         return data.astype(np.float32) if isinstance(data, np.ndarray) else np.array(data, dtype=np.float32)
+
+    def resolve(self, transform):
+        """
+        Resolves a transform function.
+
+        Args:
+            transform: transform function
+
+        Returns:
+            resolved transform function
+        """
+
+        if transform:
+            # Resolve transform instance, if necessary
+            transform = Resolver()(transform) if transform and isinstance(transform, str) else transform
+
+            # Get function or callable instance
+            transform = transform if isinstance(transform, types.FunctionType) else transform()
+
+        return transform
