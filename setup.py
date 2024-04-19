0c828b8e208e25aae82041fec2b18d17ed01548f
davidmezzetti <561939+davidmezzetti@users.noreply.github.com>
2024-04-19 15:32:23 -0400

    Bump version

--- setup.py
+++ setup.py
@@ -108,7 +108,7 @@ extras["all"] = (
 
 setup(
     name="txtai",
-    version="7.1.0",
+    version="7.2.0",
     author="NeuML",
     description="All-in-one open-source embeddings database for semantic search, LLM orchestration and language model workflows",
     long_description=DESCRIPTION,
--- src/python/txtai/version.py
+++ src/python/txtai/version.py
@@ -3,7 +3,7 @@ Version strings
 """
 
 # Current version tag
-__version__ = "7.1.0"
+__version__ = "7.2.0"
 
 # Current pickle protocol
 __pickle__ = 4
