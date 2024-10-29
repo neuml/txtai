285769c971482af9a6aa12699cb2ce71b38c5473
davidmezzetti <561939+davidmezzetti@users.noreply.github.com>
2024-10-29 13:25:58 -0400

    Update build script #805

--- test/python/testdatabase/testduckdb.py
+++ test/python/testdatabase/testduckdb.py
@@ -2,6 +2,9 @@
 DuckDB module tests
 """
 
+import os
+import unittest
+
 from txtai.embeddings import Embeddings
 
 from .testrdbms import Common
@@ -42,3 +45,11 @@ class TestDuckDB(Common.TestRDBMS):
 
         if cls.embeddings:
             cls.embeddings.close()
+
+    @unittest.skipIf(os.name == "nt", "testArchive skipped on Windows")
+    def testArchive(self):
+        """
+        Test embeddings index archiving
+        """
+
+        super().testArchive()
