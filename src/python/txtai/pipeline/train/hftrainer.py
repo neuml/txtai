ba552d5a3f9d3690b0afdda6b32670954a2619bb
davidmezzetti <561939+davidmezzetti@users.noreply.github.com>
2025-02-22 10:42:00 -0500

    Set deterministic behavior for cuDNN in HFTrainer #879

--- src/python/txtai/pipeline/train/hftrainer.py
+++ src/python/txtai/pipeline/train/hftrainer.py
@@ -89,8 +89,8 @@ class HFTrainer(Tensors):
         # Parse TrainingArguments
         args = self.parse(args)
 
-        # Set seed for model reproducibility
-        set_seed(args.seed)
+        # Initialize torch parameters for reproducibility
+        self.initialize(args.seed)
 
         # Load model configuration, tokenizer and max sequence length
         config, tokenizer, maxlength = self.load(base, maxlength)
@@ -155,19 +155,27 @@ class HFTrainer(Tensors):
         """
 
         # Default training arguments
-        args = {
-            "output_dir": "",
-            "save_strategy": "no",
-            "report_to": "none",
-            "log_level": "warning",
-            "use_cpu": not Models.hasaccelerator(),
-            "full_determinism": True,
-        }
+        args = {"output_dir": "", "save_strategy": "no", "report_to": "none", "log_level": "warning", "use_cpu": not Models.hasaccelerator()}
 
         # Apply custom arguments
         args.update(updates)
 
         return TrainingArguments(**args)
+
+    def initialize(self, seed):
+        """
+        Initializes torch parameters to ensure reproducibility.
+
+        Args:
+            seed: seed to set
+        """
+
+        # Set seed for model reproducibility
+        set_seed(seed)
+
+        # Enable cuDNN deterministic mode
+        torch.backends.cudnn.deterministic = True
+        torch.backends.cudnn.benchmark = False
 
     def load(self, base, maxlength):
         """
