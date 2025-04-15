80811ef9f367a43b8b29bbe83e00ebceb6efc810
davidmezzetti <561939+davidmezzetti@users.noreply.github.com>
2025-04-14 20:19:02 -0400

    Bump version

--- setup.py
+++ setup.py
@@ -135,7 +135,7 @@ extras["all"] = (
 
 setup(
     name="txtai",
-    version="8.5.0",
+    version="8.6.0",
     author="NeuML",
     description="All-in-one open-source embeddings database for semantic search, LLM orchestration and language model workflows",
     long_description=DESCRIPTION,
