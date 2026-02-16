0ccad01340d123040e7757367a051b0c5effa249
Hemantsudarshan <hemanthsudarshan2002@gmail.com>
2026-02-16 10:37:39 +0530

    feat(caption): add generator/iterator support for streaming image processing

--- test/python/testpipeline/testimage/testcaption.py
+++ test/python/testpipeline/testimage/testcaption.py
@@ -1,4 +1,4 @@
-"""
+﻿"""
 Caption module tests
 """
 
@@ -24,3 +24,30 @@ class TestCaption(unittest.TestCase):
 
         caption = Caption()
         self.assertEqual(caption(Image.open(Utils.PATH + "/books.jpg")), "a book shelf filled with books and a stack of books")
+
+    def testCaptionGenerator(self):
+        """
+        Test captions with generator input
+        """
+
+        caption = Caption()
+
+        def image_gen():
+            yield Utils.PATH + "/books.jpg"
+            yield Utils.PATH + "/books.jpg"
+
+        results = caption(image_gen())
+        self.assertEqual(len(results), 2)
+        for r in results:
+            self.assertIsInstance(r, str)
+            self.assertTrue(len(r) > 0)
+
+    def testCaptionIterator(self):
+        """
+        Test captions with iterator input
+        """
+
+        caption = Caption()
+        results = caption(iter([Utils.PATH + "/books.jpg", Utils.PATH + "/books.jpg"]))
+        self.assertEqual(len(results), 2)
+        self.assertEqual(results[0], results[1])
