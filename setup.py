7613acd6e5ef11b808444162239a87cf1f9b5582
davidmezzetti <561939+davidmezzetti@users.noreply.github.com>
2026-07-01 15:24:55 -0400

    Bump version

--- setup.py
+++ setup.py
@@ -176,7 +176,7 @@ extras["all"] = (
 
 setup(
     name=package,
-    version="9.11.0",
+    version="9.12.0",
     author="NeuML",
     description="All-in-one open-source AI framework for semantic search, LLM orchestration and language model workflows",
     long_description=DESCRIPTION,
